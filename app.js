f1b907e9196c0f8fbeb3800a61ca5f0067b213de
Nathan <nathan.barthelemy2204@gmail.com>
2025-09-15 22:53:26 +0200

    Réponse Statique

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // Inclusion du module 'express' pour créer le serveur web
 const express = require('express');
 
+// Inclusion du module 'path' pour gérer les chemins de fichiers
+const path = require('path');
+
+
 // Création de l'application Express
 const app = express();
 
@@ -10,6 +14,10 @@ const app = express();
 //     res.setHeader('Content-Type','text/html;charset=utf-8');
 //     res.end("Le serveur Express dit <b>bonjour</b>"); 
 // });
+
+// Servir des fichiers statiques (CSS, images, JS côté client, etc.)
+// Le dossier 'public' est utilisé pour servir les fichiers statiques
+app.use(express.static(path.join(__dirname,'public')));
 
 //Middleware
 // 1er middleware : ex. d'affichage d'informations dans la console
@@ -28,7 +36,11 @@ app.use((req,res,next)=>{
 
 // 3ème middleware : envoi de la réponse
 app.use ((req,res)=>{
-    res.end("Le serveur Express dit <b>bonjour</b>");
+    // exemple du middleware qui envoie la réponse
+    // res.end("Le serveur Express dit <b>bonjour</b>");
+
+    // Renvoyer un fichier HTML statique
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
 });
 
 // Export de l'application pour qu'elle puisse être utilisée par d'autres modules
