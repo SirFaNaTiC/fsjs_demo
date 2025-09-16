29c3e5355359f7ece79a6da1a07e9a13028ef5e3
Nathan <nathan.barthelemy2204@gmail.com>
2025-09-16 14:06:06 +0200

    Templates EJS jusqu'au npm install express-ejs-layouts

--- app.js
+++ app.js
@@ -19,28 +19,36 @@ const app = express();
 // Le dossier 'public' est utilisé pour servir les fichiers statiques
 app.use(express.static(path.join(__dirname,'public')));
 
+app.set('view engine','ejs'); // moteur de template
+app.set('views', path.join(__dirname,'views')); // dossier des vues
+
 //Middleware
 // 1er middleware : ex. d'affichage d'informations dans la console
-app.use((req,res,next)=>{
-    const now = new Date().toDateString();
-    console.log(`${now} : une requête ${req.method} est arrivée`);
-    next(); // passe la main au middleware suivant
-});
+// app.use((req,res,next)=>{
+//     const now = new Date().toDateString();
+//     console.log(`${now} : une requête ${req.method} est arrivée`);
+//     next(); // passe la main au middleware suivant
+// });
 
 // 2ème middleware : préparation de la réponse
-app.use((req,res,next)=>{
-    res.status(200);
-    res.setHeader('Content-Type','text/html;charset=utf-8');
-    next(); // passe la main au middleware suivant
-});
+// app.use((req,res,next)=>{
+//     res.status(200);
+//     res.setHeader('Content-Type','text/html;charset=utf-8');
+//     next(); // passe la main au middleware suivant
+// });
 
 // 3ème middleware : envoi de la réponse
-app.use ((req,res)=>{
-    // exemple du middleware qui envoie la réponse
-    // res.end("Le serveur Express dit <b>bonjour</b>");
+// app.use ((req,res)=>{
+//     // exemple du middleware qui envoie la réponse
+//     // res.end("Le serveur Express dit <b>bonjour</b>");
 
-    // Renvoyer un fichier HTML statique
-    res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
+//     // Renvoyer un fichier HTML statique
+//     res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
+// });
+
+// Routeur pour la page d'accueil
+app.use((req,res)=>{
+    res.render('pages/home',{user: {sex:'homme', nickname: 'Nate'}});
 });
 
 // Export de l'application pour qu'elle puisse être utilisée par d'autres modules
